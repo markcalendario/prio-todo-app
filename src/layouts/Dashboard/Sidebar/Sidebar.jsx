3fed001da08e548206291490113cdd1c5612174b
markcalendario <markcalendario@gmail.com>
2024-08-02 12:35:35 +0800

    feat: active sidebar

--- src/layouts/Dashboard/Sidebar/Sidebar.jsx
+++ src/layouts/Dashboard/Sidebar/Sidebar.jsx
@@ -6,9 +6,13 @@ export default function Sidebar() {
     <aside id="dashboard-sidebar" className="sidebar">
       <p className="label">Tasks</p>
       <SidebarLink href="/" icon="fas fa-clock" text="Pending" />
-      <SidebarLink href="important" icon="fas fa-fire" text="Important" />
-      <SidebarLink href="finished" icon="fas fa-check-circle" text="Finished" />
-      <SidebarLink href="missing" icon="fas fa-warning" text="Missing" />
+      <SidebarLink href="/important" icon="fas fa-fire" text="Important" />
+      <SidebarLink
+        href="/finished"
+        icon="fas fa-check-circle"
+        text="Finished"
+      />
+      <SidebarLink href="/missing" icon="fas fa-warning" text="Missing" />
 
       <hr />
 
--- src/layouts/Dashboard/Sidebar/SidebarLink/SidebarLink.jsx
+++ src/layouts/Dashboard/Sidebar/SidebarLink/SidebarLink.jsx
@@ -1,8 +1,15 @@
+import { useLocation } from "react-router-dom";
 import "./SidebarLink.css";
 
 export default function SidebarLink({ href, icon, text, target }) {
+  const { pathname } = useLocation();
+  const isActive = pathname === href;
+
   return (
-    <a href={href} className="link" target={target}>
+    <a
+      href={href}
+      className={"link" + (isActive ? " active" : "")}
+      target={target}>
       <i className={"icon fa-fw" + (icon ? ` ${icon}` : "")} />
       <span className="text">{text}</span>
     </a>
