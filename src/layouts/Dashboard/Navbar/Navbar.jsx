50315ba7ef03a31284a27b1d96ce992df91c1a02
markcalendario <markcalendario@gmail.com>
2024-08-05 07:02:24 +0800

    refactor: adjust navbar button

--- src/layouts/Dashboard/Navbar/Navbar.jsx
+++ src/layouts/Dashboard/Navbar/Navbar.jsx
@@ -5,11 +5,14 @@ export default function Navbar({ toggleSidebarVisibility }) {
   return (
     <nav className="navbar">
       <div className="fluid-container">
+        <div className="hamburger">
+          <IconButton
+            icon="fas fa-bars fa-fw"
+            onClick={toggleSidebarVisibility}
+          />
+        </div>
         <div className="brand">
           <h1>Prio.</h1>
-        </div>
-        <div className="hamburger">
-          <IconButton icon="fas fa-bars" onClick={toggleSidebarVisibility} />
         </div>
       </div>
     </nav>
